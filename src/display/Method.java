e0e82474674b4f09cb2f204bbd546d0b31838411
YanaiPhone <s12547@salesio-sp.ac.jp>
2016-05-06 22:05:22 +0900

    peerを消した

--- src/display/Method.java
+++ src/display/Method.java
@@ -5,7 +5,6 @@ import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.peer.PanelPeer;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
