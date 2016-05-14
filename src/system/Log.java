eca9f2db86b95d93ba1487f7bd3754840403f7be
Shinichi Yanagido <hakuto1yds@gmail.com>
2016-05-14 14:49:49 +0900

    ログクラスの概念作成

---
+++ src/system/Log.java
@@ -0,0 +1,19 @@
+package system;
+
+class Log {
+    // TODO:ログの書き出し
+    public static void writeLog() {
+    }
+
+    // TODO:エラーの書き出し
+    public static void writeError() {
+    }
+
+    // TODO:不正の書き出し
+    public static void writeCorruption() {
+    }
+
+    // TODO:ポップアップによるエラー
+    public static void showError() {
+    }
+}
