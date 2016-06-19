fae8135a3be2d26054f340cae3e0326ed179d479
Shinichi Yanagido <hakuto1yds@gmail.com>
2016-06-19 17:59:11 +0900

    Agendaクラスに対してjavadocを追加

--- src/system/Agenda.java
+++ src/system/Agenda.java
@@ -2,7 +2,11 @@ package system;
 
 import java.lang.String;
 
-// TODO:各月の予定を1ヶ月分記録
+/**
+ * 各月の予定を一月分保持するクラス
+ * @author Shinichi Yanagido
+ * @version 0.1
+ */
 public class Agenda {
 	// Stirng message
 	// int month
